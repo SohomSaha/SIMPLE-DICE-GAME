16e60c8bf7e0fd0cf044ce12c623134a3de3848c
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-06-23 16:54:43 +0530

    Update index.js
    Adding name feature of players

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var player1=prompt("Write name of Player 1");
+var player2=prompt("Write name of Player 2");
 function play(){
 var r1=Math.floor(Math.random()*6)+1;
 var image1="images/dice"+r1+".png";
@@ -6,9 +8,9 @@ var r2=Math.floor(Math.random()*6)+1;
 var image2="images/dice"+r2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",image2);
 if(r1>r2)
-document.querySelector("h1").innerHTML="Player 1 wins💪💪";
+document.querySelector("h1").innerHTML=player1+" wins💪💪";
 else if(r1<r2)
-document.querySelector("h1").innerHTML="Player 2 wins💪💪";
+document.querySelector("h1").innerHTML=player2+" wins💪💪";
 else
 document.querySelector("h1").innerHTML="DRAW🤝🤝";
 document.querySelector("button").innerHTML="Continue";
