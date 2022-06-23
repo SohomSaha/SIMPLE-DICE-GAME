8f1178eac98db27b3b6d77d4f24ad6550057d60e
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-06-23 14:10:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function play(){
+var r1=Math.floor(Math.random()*6)+1;
+var image1="images/dice"+r1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",image1);
+var r2=Math.floor(Math.random()*6)+1;
+var image2="images/dice"+r2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",image2);
+if(r1>r2)
+document.querySelector("h1").innerHTML="Player 1 wins💪💪";
+else if(r1<r2)
+document.querySelector("h1").innerHTML="Player 2 wins💪💪";
+else
+document.querySelector("h1").innerHTML="DRAW🤝🤝";
+document.querySelector("button").innerHTML="Continue";
+
+}
